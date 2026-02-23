c737b082e0f231543560beab6d4ecdd8c423570a
gordoste <gordoste@iinet.net.au>
2026-02-23 23:11:09 +1100

    Formatting only

--- src/led_map.c
+++ src/led_map.c
@@ -46,12 +46,13 @@ uint16_t moonNumToMapNum(uint16_t moonNum, uint8_t numRows, uint8_t numCols)
 }
 
 #ifndef WIRING_CUSTOM
-void populate_led_map(uint16_t led_map[], uint8_t numRows, uint8_t numCols) {
-   	bool bWiringGoesDownThisCol = !bWiringFirstLEDBottom;
+void populate_led_map(uint16_t led_map[], uint8_t numRows, uint8_t numCols)
+{
+	bool bWiringGoesDownThisCol = !bWiringFirstLEDBottom;
 	uint8_t ledsInCol = numRows + ledSkipNum; // number of LEDs in each column including skipped LEDs
 
 	uint16_t ledNum = 0;
-	int col = bWiringFirstLEDRight ? (numCols - 1) : 0; // current column we are filling in the LED map
+	int col = bWiringFirstLEDRight ? (numCols - 1) : 0;	  // current column we are filling in the LED map
 	int row = bWiringGoesDownThisCol ? (numRows - 1) : 0; // current row we are filling in the LED map
 	while (col < numCols && col >= 0)
 	{
@@ -62,26 +63,31 @@ void populate_led_map(uint16_t led_map[], uint8_t numRows, uint8_t numCols) {
 			row += bWiringGoesDownThisCol ? -1 : 1;
 			// Move to the next LED in the chain, skipping any LEDs that need to be skipped
 			ledNum++;
-			uint8_t ledNumInChunk = ledNum % (2*ledsInCol);
+			uint8_t ledNumInChunk = ledNum % (2 * ledsInCol);
 			// The first clause is for columns 1,3,5, etc. , second clause is for columns 2,4,6, etc. since the wiring goes down one column and up the next
-			while ((ledNumInChunk < ledsInCol && ledSkip(ledNumInChunk)) || (ledNumInChunk > ledsInCol && ledSkip(2*ledsInCol - 1 - ledNumInChunk))) {
+			while ((ledNumInChunk < ledsInCol && ledSkip(ledNumInChunk)) || (ledNumInChunk > ledsInCol && ledSkip(2 * ledsInCol - 1 - ledNumInChunk)))
+			{
 				ledNum++;
-				ledNumInChunk = ledNum % (2*ledsInCol);
+				ledNumInChunk = ledNum % (2 * ledsInCol);
 			}
 		}
 		col += bWiringFirstLEDRight ? -1 : 1;
-        if (row == numRows) {
-            row = numRows - 1;
-        } else if (row == -1) {
-            row = 0;
-        }
+		if (row == numRows)
+		{
+			row = numRows - 1;
+		}
+		else if (row == -1)
+		{
+			row = 0;
+		}
 		bWiringGoesDownThisCol = !bWiringGoesDownThisCol;
 	}
 }
 #else
-void populate_led_map_custom(uint16_t led_map[], uint8_t numRows, uint8_t numCols) {
-    // If you want to define your own wiring configuration, you can do that here
-    // The led_map[x*numRows + y] array needs to contain the LED number that should be used for the
-    // hold at row x, column y. Rows are number L-to-R and columns are numbered from the bottom up.
+void populate_led_map_custom(uint16_t led_map[], uint8_t numRows, uint8_t numCols)
+{
+	// If you want to define your own wiring configuration, you can do that here
+	// The led_map[x*numRows + y] array needs to contain the LED number that should be used for the
+	// hold at row x, column y. Rows are number L-to-R and columns are numbered from the bottom up.
 }
 #endif
--- src/led_map.h
+++ src/led_map.h
@@ -4,19 +4,19 @@
 // Wiring must be zig-zag fashion up and down the columns.
 // By default, we have the first LED at the top of the first column, and the columns go left to right
 // Uncommenting the following defines can modify this
-#define WIRING_FIRST_LED_RIGHT 			// Columns goes right to left instead
-//#define WIRING_FIRST_LED_BOTTOM			// First LED is at the bottom of the first column instead of the top
+#define WIRING_FIRST_LED_RIGHT              // Columns goes right to left instead
+// #define WIRING_FIRST_LED_BOTTOM			// First LED is at the bottom of the first column instead of the top
 
 // If some LEDs need to be skipped because there is a beam or other obstruction that you need to go around/over,
 // you can control that here. You need to specify the LED numbers that will be skipped in the first column and the code
 // will assume the other columns are identical.
 // For example, if you have a beam between the 6th and 7th row, and another between the 12th and 13th row, and you need
 // to skip 2 LEDs for each beam, you'd specify 6,7,14,15 so that LEDs 0-5, 8-13, 16-21 are used for the first column
-//#define WIRING_LEDS_SKIP_FIRST_COLUMN 6,7,14,15
+// #define WIRING_LEDS_SKIP_FIRST_COLUMN 6,7,14,15
 
 // If you want to define your own wiring configuration, you need to unccmment the following line and implement populate_led_map()
 // in led_map.c
-//#define WIRING_CUSTOM
+// #define WIRING_CUSTOM
 
 #include <stdbool.h>
 #include <stddef.h>
