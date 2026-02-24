2ea4b0614d2093a16d65483fe136b9ff7e6b7fa4
gordoste <gordoste@iinet.net.au>
2026-02-24 22:41:24 +1100

    [tested] Simplify moonNumToMapNum()

--- src/led_map.c
+++ src/led_map.c
@@ -38,15 +38,20 @@ bool ledSkip(uint16_t ledNum)
 
 uint16_t moonNumToMapNum(uint16_t moonNum)
 {
+	int iFullCols = moonNum / NUM_ROWS;
+	int iLEDsRemaining = moonNum % NUM_ROWS;
 	// We get passed the number of the LED in the standard wiring, which has LED #0 at the bottom-left
-	// First we calculate how many complete pairs of 2-columns are prior to the given LED...
-	int numColPairs = moonNum / (2 * NUM_ROWS);
-	// ... and how many LEDs remain
-	int numLEDsRemaining = moonNum % (2 * NUM_ROWS);
-	// Our LED map has the columns in sequence, from bottom to top
-	// If we have less than a full column, then the column that the LED is in will be wired upwards so we can just add that number
-	// If we have more than a full column left, then add the full column, and then calculate how far the LED is from the bottom and add that
-	return (numColPairs * 2 * NUM_ROWS) + (numLEDsRemaining < NUM_ROWS ? numLEDsRemaining : NUM_ROWS + (2 * NUM_ROWS - 1 - numLEDsRemaining));
+	// Our LED map has the columns in sequence, with every column numbered from bottom to top
+	// If the LED is in an even column (0-indexed), the numbers will be the same.
+	// If the LED is in an odd column (0-indexed), the numbers will be reversed since the wiring goes down the next column
+	if (iFullCols % 2 == 0)
+	{
+		return moonNum;
+	}
+	else
+	{
+		return (iFullCols * NUM_ROWS) + (NUM_ROWS - 1 - iLEDsRemaining);
+	}
 }
 
 #ifndef WIRING_CUSTOM
