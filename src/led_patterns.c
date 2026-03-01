f3ffc452df0a51ae15e7b82c1c079694232ae1b2
gordoste <gordoste@iinet.net.au>
2026-03-01 22:00:18 +1100

    Shorten led_startup_pattern()

--- src/led_patterns.c
+++ src/led_patterns.c
@@ -8,7 +8,7 @@ led_pattern_fn_t led_patterns[] = {
 
 void led_startup_pattern(void)
 {
-    for (int c = 0; c < 100; c++) // flash all LEDs in sequence to show we're alive and have Bluetooth working
+    for (int c = 0; c < 40; c++) // flash all LEDs in sequence to show we're alive and have Bluetooth working
     {
         for (int r = 0; r < NUM_ROWS; r++)
         {
