980815cb2959022c5c188a40bdf2d84ce5d6fdd9
gordoste <gordoste@iinet.net.au>
2026-03-01 22:37:08 +1100

    Add more patterns, implement show_random_pattern()

--- src/led_patterns.c
+++ src/led_patterns.c
@@ -4,7 +4,13 @@
 LOG_MODULE_REGISTER(led_patterns);
 
 led_pattern_fn_t led_patterns[] = {
-    led_startup_pattern};
+    led_startup_pattern,
+    left_to_right_pattern,
+    right_to_left_pattern,
+    top_to_bottom_pattern,
+    bottom_to_top_pattern};
+
+#define NUM_LED_PATTERNS (sizeof(led_patterns) / sizeof(led_patterns[0]))
 
 void led_startup_pattern(void)
 {
@@ -18,6 +24,79 @@ void led_startup_pattern(void)
         if (err)
         {
             LOG_ERR("Failed to update LED strip: %d", err);
+            return;
+        }
+        k_sleep(K_MSEC(100));
+    }
+}
+
+void left_to_right_pattern(void)
+{
+    for (int c = 0; c < NUM_COLS; c++)
+    {
+        clearStrip(false);
+        for (int r = 0; r < NUM_ROWS; r++) {
+            LED_SET_PIXEL(c, r, color_list[r % NUM_COLORS]);
+        }
+        int err = led_strip_update_rgb(strip, pixels, STRIP_LENGTH);
+        if (err)
+        {
+            LOG_ERR("Failed to update LED strip: %d", err);
+            return;
+        }
+        k_sleep(K_MSEC(100));
+    }
+}
+
+void right_to_left_pattern(void)
+{
+    for (int c = NUM_COLS - 1; c >= 0; c--)
+    {
+        clearStrip(false);
+        for (int r = 0; r < NUM_ROWS; r++) {
+            LED_SET_PIXEL(c, r, color_list[r % NUM_COLORS]);
+        }
+        int err = led_strip_update_rgb(strip, pixels, STRIP_LENGTH);
+        if (err)
+        {
+            LOG_ERR("Failed to update LED strip: %d", err);
+            return;
+        }
+        k_sleep(K_MSEC(100));
+    }
+}
+
+void top_to_bottom_pattern(void)
+{
+    for (int r = NUM_ROWS - 1; r >= 0; r--)
+    {
+        clearStrip(false);
+        for (int c = 0; c < NUM_COLS; c++) {
+            LED_SET_PIXEL(c, r, color_list[c % NUM_COLORS]);
+        }
+        int err = led_strip_update_rgb(strip, pixels, STRIP_LENGTH);
+        if (err)
+        {
+            LOG_ERR("Failed to update LED strip: %d", err);
+            return;
+        }
+        k_sleep(K_MSEC(100));
+    }
+}
+
+void bottom_to_top_pattern(void)
+{
+    for (int r = 0; r < NUM_ROWS; r++)
+    {
+        clearStrip(false);
+        for (int c = 0; c < NUM_COLS; c++) {
+            LED_SET_PIXEL(c, r, color_list[c % NUM_COLORS]);
+        }
+        int err = led_strip_update_rgb(strip, pixels, STRIP_LENGTH);
+        if (err)
+        {
+            LOG_ERR("Failed to update LED strip: %d", err);
+            return;
         }
         k_sleep(K_MSEC(100));
     }
@@ -25,4 +104,6 @@ void led_startup_pattern(void)
 
 void show_random_pattern(void)
 {
+    int pattern_index = rand() % NUM_LED_PATTERNS;
+    led_patterns[pattern_index]();
 }
--- src/led_patterns.h
+++ src/led_patterns.h
@@ -1,7 +1,13 @@
 #include "zboard.h"
 
-void led_startup_pattern(void);
+#include <stdlib.h>
 
 typedef void (*led_pattern_fn_t)(void);
+
+void led_startup_pattern(void);
+void left_to_right_pattern(void);
+void right_to_left_pattern(void);
+void top_to_bottom_pattern(void);
+void bottom_to_top_pattern(void);
 
 void show_random_pattern(void);
